48c4a4d33ef57d91cd50e4fb5422d83509fc5887
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2025-02-20 20:00:20 +0600

    added functionality to save user data

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("TaskOrbit-DB");
+    const userCollection = db.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
